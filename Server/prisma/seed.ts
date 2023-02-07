4ecf6e4939f399d85be70515342e3568d5a8fc1f
Higor Hungria <higor.hungria466@gmail.com>
2023-02-07 14:31:17 -0300

    Adicionado seed do fornecedor
    Alimentando a seed do fornecedor

--- Server/prisma/seed.ts
+++ Server/prisma/seed.ts
@@ -4,8 +4,10 @@ const prisma = new PrismaClient();
 
 async function run(){
     /*Delete*/await Promise.all([
+        prisma.endereco.deleteMany(),
         prisma.cliente.deleteMany(),
-        prisma.endereco.deleteMany()
+        prisma.endereco_Fornecedor.deleteMany(),
+        prisma.fornecedor.deleteMany()
     ])
     /*Create cliente*/ await Promise.all([
         prisma.cliente.create({
@@ -59,6 +61,71 @@ async function run(){
                 email: 'breno@gmail.com',
                 inscricaoEstadual: 'MG',
                 Endereco:{
+                    create:{
+                        id: 3,
+                        cep: '33333-333',
+                        cidade: 'Belo Horizonte',
+                        estado: 'MG',
+                        logradouro: 'Rua 3',
+                        numero: 1003,
+                        complemento: 'esquina'
+                    }
+                }
+            }
+        }),
+    ])
+    /*Create fornecedor */ await Promise.all([
+        prisma.fornecedor.create({
+            data:{
+                id: 1,
+                razaoSocial: 'HigorEmpresa',
+                cnpj: '111111111-11',
+                telefone: '(11)91111-1111',
+                email: 'higor@gmail.com',
+                inscricaoEstadual: 'SP',
+                Endereco_Fornecedor:{
+                    create: {
+                        id: 1,
+                        cep: '11111-111',
+                        cidade: 'São Paulo',
+                        estado: 'SP',
+                        logradouro: 'Rua 1',
+                        numero: 1001,
+                        complemento: 'esquina'   
+                    }
+                }
+            }
+        }),
+        prisma.fornecedor.create({
+            data:{
+                id: 2,
+                razaoSocial: 'GabrielEmpresa',
+                cnpj: '222222222-22',
+                telefone: '(22)92222-2222',
+                email: 'gabriel@gmail.com',
+                inscricaoEstadual: 'RJ',
+                Endereco_Fornecedor:{
+                    create:{
+                        id: 2,
+                        cep: '22222-222',
+                        cidade: 'Rio de Janeiro',
+                        estado: 'RJ',
+                        logradouro: 'Rua 2',
+                        numero: 1002,
+                        complemento: 'esquina'
+                    }
+                }
+            }
+        }),
+        prisma.fornecedor.create({
+            data:{
+                id: 3,
+                razaoSocial: 'BrenoEmpresa',
+                cnpj: '333333333-33',
+                telefone: '(33)93333-3333',
+                email: 'breno@gmail.com',
+                inscricaoEstadual: 'MG',
+                Endereco_Fornecedor:{
                     create:{
                         id: 3,
                         cep: '33333-333',
