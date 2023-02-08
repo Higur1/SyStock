dcbcf22c9e193d8d1e903acc9d4818931935aecd
Gabriel Akio <akioumebayashi@gmail.com>
2023-02-07 21:37:04 -0300

    build-front: creating starter build

---
+++ Front/systock/src/App.js
@@ -0,0 +1,14 @@
+import Master from './Master.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      {/* <Sidebar /> */}
+      <div style={{ background: '#000000'}}>Sidebar will be here</div>
+      <Master />
+    </div>
+  );
+}
+
+export default App;
