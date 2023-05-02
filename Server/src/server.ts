7d63223927ff9dbb5f4cd4277b00c26e73120268
Higor Hungria <higor.hungria466@gmail.com>
2023-05-02 15:44:56 -0300

    Swagger Config
    Swagger options with documentation

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,24 +2,33 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import dotenv from "dotenv";
 import ip from "ip";
-//import { rotasCliente } from "./rotasCliente";
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
+import { SwaggerTheme } from 'swagger-themes'
 import { supplier_routes } from "./routes/supplier_routes";
 import { category_routes } from "./routes/category_routes";
 import { product_routes } from "./routes/product_routes";
-import { user_routes } from "./routes/user_routes";
+import { user_routes } from "./routes/users_routes";
+import { userSchemas } from "./routes/user.schema";
 
 dotenv.config();
 
 const app = fastify();
 const port: number = Number(process.env.PORT);
 
+await app.register(swagger, {
+  openapi: {}
+})
+app.register(swaggerUi, {
+  theme:{
+    css:[
+      {filename: 'theme.css', content: new SwaggerTheme('v3').getBuffer('dark')}
+    ],
+  }
+})
 app.register(cors, {
   origin: "*",
 });
-
-//Swagger config
-const Swagger = require('./config/swaggerOptions');
-app.register(require('fastify-swagger'), Swagger.options);
 
 //app.register(rotasCliente);
 app.register(supplier_routes);
@@ -27,10 +36,14 @@ app.register(category_routes);
 app.register(product_routes);
 app.register(user_routes);
 
+
+
 const start = async () => {
   try {
+    for(const schema of [...userSchemas]){
+      app.addSchema(schema);
+    }
     await app.listen({ port: port, host: ip.address() });
-    app.swagger();
     console.log(`Served at ${ip.address()}:${port}`);
   } catch (error) {
     app.log.error(error);
