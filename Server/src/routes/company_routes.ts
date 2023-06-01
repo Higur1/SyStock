ca9f2bf229070f50e7ed57f17b5843f0c61c7b8d
BrenoZacharias <brenomzacharias@gmail.com>
2023-06-01 16:56:47 -0300

    add crud companyPhone

--- Server/src/routes/company_routes.ts
+++ Server/src/routes/company_routes.ts
@@ -78,7 +78,7 @@ export async function company_routes(app: FastifyInstance) {
   });
   app.get("/companies", async (request, response) => {
     try {
-      await prisma.$queryRaw`SELECT C.*,A.* FROM company C INNER JOIN company_Address A ON C.id == A.company_id`.then(
+      await prisma.$queryRaw`SELECT C.*,A.*, P* FROM company C INNER JOIN company_Address A ON C.id == A.company_id INNER JOIN company_Phone P ON P.company_id == C.id`.then(
         async (company) => {
           if (!company) {
             response.status(404).send("Not found");
@@ -236,6 +236,169 @@ export async function company_routes(app: FastifyInstance) {
               });
               response.status(200);
             });
+        });
+    } catch (error) {
+      response.status(400).send(
+        JSON.stringify({
+          error: error.meta.target,
+          message: "An error has occurred",
+        })
+      );
+    }
+  });
+
+  app.post("/companiesPhone/new", async (request, response) => {
+    const company = z.object({
+      phone: z.string(),
+      company_id: z.number(),
+    });
+    const { phone, company_id } = company.parse(request.body);
+
+    try {
+      await prisma.company_Phone
+        .findFirst({
+          where: {
+            phone: phone,
+          },
+        })
+        .then(async (companyPhone) => {
+          if (companyPhone) {
+            response.status(200).send("an operation could not be performed");
+          }
+          await prisma.company_Phone
+            .create({
+              data: {
+                phone: phone,
+                company_id: company_id,
+              },
+            })
+            .then(async (companyPhone) => {
+              response.status(201).send(companyPhone);
+            });
+        });
+    } catch (error) {
+      response.status(400).send(
+        JSON.stringify({
+          error: error,
+          message: "An error has occurred",
+        })
+      );
+    }
+  });
+  app.get("/companiesPhone", async (request, response) => {
+    try {
+      await prisma.company_Phone.
+      findMany()
+      .then(async (companyPhoneList) => {
+        if (Object.keys(companyPhoneList).length === 0) {
+          response.status(200).send("Empty");
+        }
+        response.status(200).send(companyPhoneList);
+      });
+    } catch (error) {
+      response.status(400).send(
+        JSON.stringify({
+          error: error.meta.target,
+          message: "An error has occurred",
+        })
+      );
+    }
+  });
+  app.get("/companiesPhone/findByCompanies/:company_id", async (request, response) => {
+    const company = z.object({
+      company_id: z.string(),
+    });
+    const { company_id } = company.parse(request.params);
+    try {
+      await prisma.company_Phone.
+      findMany({
+        where: {
+          company_id: Number(company_id),
+        },
+      })
+      .then(async (companyPhoneList) => {
+        if (!companyPhoneList) {
+          response.status(200).send("Not found");
+        }
+        response.status(200).send(companyPhoneList);
+      });
+    } catch (error) {
+      response.status(400).send(
+        JSON.stringify({
+          error: error.meta.target,
+          message: "An error has occurred",
+        })
+      );
+    }
+  });
+  app.put("/companiesPhone/update", async (request, response) => {
+    const companyPhone = z.object({
+      id: z.number(),
+      phone: z.string(),
+    });
+    const { id, phone } = companyPhone.parse(request.body);
+
+    try {
+      await prisma.company_Phone
+        .findUnique({
+          where: {
+            id: id,
+          },
+        })
+        .then(async (companyPhone) => {
+          if (!companyPhone) {
+            response.status(200).send("Not found");
+          }
+          await prisma.company_Phone
+            .findFirst({
+              where: {
+                phone: phone,
+              },
+            })
+            .then(async (phoneExists) => {
+              if (phoneExists) {
+                response.status(200).send("Phone alredy exists");
+              }
+              await prisma.company_Phone
+                .update({
+                  where: { id: id },
+                  data: {
+                    phone: phone,
+                  },
+                })
+                .then(async (companyPhone) => {
+                  response.status(200).send(companyPhone);
+                });
+            });
+        });
+    } catch (error) {
+      response.status(400).send(
+        JSON.stringify({
+          error: error.meta.target,
+          message: "An error has occurred",
+        })
+      );
+    }
+  });
+  app.delete("/companiesPhone/delete", async (request, response) => {
+    const companyPhone_id = z.object({
+      id: z.number(),
+    });
+    const { id } = companyPhone_id.parse(request.body);
+
+    try {
+      await prisma.company_Phone
+        .findUnique({
+          where: { id: id },
+        })
+        .then(async (companyPhone) => {
+          if (!companyPhone) {
+            response.status(200).send("Not found");
+          }
+          await prisma.company_Phone.delete({
+            where: { id: id }
+          })
+          response.status(200).send(companyPhone);
         });
     } catch (error) {
       response.status(400).send(
