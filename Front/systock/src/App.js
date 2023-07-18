893a181bf1efc5540cb90ac07209c5ac0aa89493
Gabriel Akio <akioumebayashi@gmail.com>
2023-07-18 00:39:16 -0300

    SD-2:

--- Front/systock/src/App.js
+++ Front/systock/src/App.js
@@ -2,10 +2,14 @@ import Master from './Master.js';
 import './App.css';
 import Sidebar from './pages/Sidebar/Sidebar.js';
 import Login from './pages/Login/Login.js';
+import { AppBar } from '@mui/material';
 
 function App() {
 
-  const isLoggedIn = false;
+  // const isLoggedIn = window.localStorage.getItem('tokenLogin');
+  const isLoggedIn = true;
+
+
   return (
     isLoggedIn ? (
       <div className='main'>
