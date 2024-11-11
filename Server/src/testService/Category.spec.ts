fe351b1b86a566f6d0603b35f7f8c02d6ddff753
Higur1 <Higor.hungria466@gmail.com>
2024-11-10 23:19:46 -0300

    Test category working

--- Server/src/testService/Category.spec.ts
+++ Server/src/testService/Category.spec.ts
@@ -10,13 +10,15 @@ import {
   beforeEach,
 } from "@jest/globals";
 
+
+
 describe("Create category model", () => {
   let categoryId;
   let UniqueName;
   let UniqueNameUpdate;
 
   beforeAll(() => {
-    const genarateUniqueCategoryName = `Mock Category-${String(Date.now())}`;
+    const genarateUniqueCategoryName = `Mock Category-${String(Date.now())}`
     UniqueName = genarateUniqueCategoryName;
   });
 
@@ -26,52 +28,52 @@ describe("Create category model", () => {
     };
 
     const createCategory = await CategoryService.create(categoryData);
-    categoryId = createCategory.category!.id;
-    await expect(createCategory).toHaveProperty("category.category_id");
+    await expect(createCategory.category).toHaveProperty("id");
   });
 
   it("Should not be able ot create a new category", async () => {
     const categoryData: ICategory = {
       name: UniqueName,
     };
-
-    const createCategory = await CategoryService.create(categoryData);
-    await expect(createCategory.error).toBe(
-      "Já existe categoria com esse nome"
-    );
+    await expect(async () => await CategoryService.create(categoryData)).rejects.toThrowError("Category Already exists")
   });
 
   it("Dado uma categoria já existente Quando seu nome é alterado para um nome que não existe no BD Então o nome da categoria recebe seu novo nome", async () => {
-    const genarateUniqueCategoryNameUpdated = `Update Mock Category-${String(
-      Date.now()
-    )}`;
-    UniqueNameUpdate = genarateUniqueCategoryNameUpdated;
-    const categoryDataUpdate: ICategory = {
-      id: categoryId,
-      name: UniqueNameUpdate,
+    const genarateUniqueCategoryNameUpdated =  `Mock Category-${String(Date.now())}`
+    const generateNewName = `Update Mock Category-${String(Date.now())}`
+
+    const categoryDataCreate: ICategory = {
+      name: genarateUniqueCategoryNameUpdated,
     };
 
-    const updatedCategory = await CategoryService.edit(categoryDataUpdate);
-    await expect(updatedCategory.category?.name).toBe(UniqueNameUpdate);
+    const createResult = await CategoryService.create(categoryDataCreate)
+
+    const newCategoryData: ICategory = {
+      id: createResult.category?.id,
+      name: generateNewName
+    };
+
+    const editResult = await CategoryService.edit(newCategoryData)
+
+    await expect(editResult.category!.name).toBe(generateNewName);
+ 
   });
 
   it("Dado uma categoria já existente Quando seu nome é alterado para um nome que já existe no BD Então é retornado o seguinte erro: 'Já existe uma categoria com esse nome'", async () => {
     const genarateUniqueCategoryName = `Mock Category-${String(Date.now())}`;
-    UniqueName = genarateUniqueCategoryName;
     const categoryData: ICategory = {
-      name: UniqueName,
+      name: genarateUniqueCategoryName,
     };
 
     const createCategory = await CategoryService.create(categoryData);
+    const findCategory = await CategoryService.findByName(UniqueName);
 
-    categoryData.name = UniqueNameUpdate;
+    const newCategoryData: ICategory = {
+      id: createCategory.category?.id,
+      name: findCategory.category!.name || ""
+    };
 
-    categoryData.id = createCategory.category?.id;
-
-    const updatedCategory = await CategoryService.edit(categoryData);
-    await expect(updatedCategory.error).toBe(
-      "Já existe categoria com esse nome"
-    );
+  await expect (async () => await CategoryService.edit(newCategoryData)).rejects.toThrowError("Name of category already exists")
   });
 
   it("Should be able to delete a category", async () => {
@@ -79,23 +81,26 @@ describe("Create category model", () => {
       id: categoryId,
       name: UniqueName,
     };
+    const newCategoryData:  ICategory ={
+      name: `Mock Category-${String(Date.now())}`
+    };
+    const createResult = await CategoryService.create(newCategoryData)
+    newCategoryData.id = createResult.category?.id;
 
-    const deletedCategory = await CategoryService.delete(categoryData);
+    const deletedCategory = await CategoryService.delete(newCategoryData);
 
     await expect(deletedCategory.status).toBe(true);
   });
 
   it("Should not be able to delete a category", async () => {
-    const categoryData: ICategory = {
-      id: 50000,
-      name: UniqueName,
+    const newCategoryData: ICategory = {
+      id: 9999999,
+      name: "Name not exists"
     };
-
-    const deletedCategory = await CategoryService.delete(categoryData);
-
-    await expect(deletedCategory.error).toBe(
-      "Não existe Categoria com o ID informado"
+    await expect(CategoryService.delete(newCategoryData)).rejects.toThrowError(
+      "Category doesn't found"
     );
+
   });
 
   afterAll(async () => {
